88fd92082f37b8e32a4cafd7f85fbda0e9908e2b
ErrorNoName <ErrorNoName666@proton.me>
2025-12-05 18:40:02 +0100

    Delete SolarisHub directory

--- SolarisHub/test.lua
+++
@@ -1,7 +0,0 @@
--- Généré par Solaris UI Designer
-local SolarisUi = require(path_to_SolarisUi)
-
-elem1 = SolarisUi.CreateFrame(UDim2.new(0,100,0,40), UDim2.new(0, 151, 0, 111), Color3.fromRGB(40,40,40), 0)
-elem2 = SolarisUi.CreateTextLabel('Label', UDim2.new(0,100,0,40), UDim2.new(0, 150, 0, 155), Color3.fromRGB(255,255,255), Enum.Font.SourceSans, 0)
-elem3 = SolarisUi.CreateButton('Button', UDim2.new(0,100,0,40), UDim2.new(0, 156, 0, 201), Color3.fromRGB(40,40,40), Color3.fromRGB(255,255,255), Enum.Font.SourceSans)
--- Ajoutez vos parents et hiérarchies ici
